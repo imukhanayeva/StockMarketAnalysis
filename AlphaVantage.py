482b2653158336588ce30e4a4ef755cbd60d564c
Indira <33145604+imukhanayeva@users.noreply.github.com>
2020-12-25 12:03:29 -0600

    Update AlphaVantage.py

--- AlphaVantage.py
+++ AlphaVantage.py
@@ -8,17 +8,24 @@ from datetime import datetime
 import pandas_datareader.data as web
 
 
-historical_time_series_data = web.DataReader("TSLA", "av-daily", start=datetime(2020,11, 23), end=datetime(2020, 12, 23),
-api_key='OG5JH42JVFCIUNS8')
+##historical_time_series_data = web.DataReader("TSLA", "av-daily", start=datetime(2020,11, 23), end=datetime(2020, 12, 23),
+##api_key='OG5JH42JVFCIUNS8')
 
-historical_time_series_data.loc["2020-11-30"]
+##historical_time_series_data.loc["2020-11-30"]
 
-web.get_quote_av(["TSLA"])
+##web.get_quote_av(["TSLA"])
 
 ####ImmediateDeprecationError: 
 AVQuotesReader has been immediately deprecated due to large breaks in the API without the
 introduction of a stable replacement. Pull Requests to re-enable these data
 connectors are welcome.
+
+stock = ['TSLA']
+start = datetime.datetime(2020,11, 23)
+end = datetime.datetime(2020,12, 23)
+data = web.DataReader(stock,data_source='yahoo',start = start, end = end)['Adj Close']
+print(data.head)
+
 
 
 
